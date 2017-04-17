06cf9c1666d412f51aed269439f2ff773e04203a
otedikova <Olga.Tedikova@returnonintelligence.com>
2017-04-17 15:31:05 +0300

    Mock search method

--- src/main/java/com/roi/poc/controller/SearchController.java
+++ src/main/java/com/roi/poc/controller/SearchController.java
@@ -1,14 +1,28 @@
 package com.roi.poc.controller;
 
+import com.roi.poc.dto.CandidateDto;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 @RestController
 @EnableAutoConfiguration
+@RequestMapping("/candidates")
 public class SearchController {
-    @RequestMapping("/")
-    String home() {
-        return "Hello World!";
+    @RequestMapping(value = "/name_search", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Collection<CandidateDto> searchCandidatesByName(@RequestParam String query) {
+        List<CandidateDto> candidateDtos = new ArrayList<CandidateDto>();
+        CandidateDto vasya = new CandidateDto();
+        vasya.setFirstName("Vasya");
+        vasya.setLastName("Pupkin");
+        candidateDtos.add(vasya);
+        return candidateDtos;
     }
 }
---
+++ src/main/java/com/roi/poc/dto/CandidateDto.java
@@ -0,0 +1,22 @@
+package com.roi.poc.dto;
+
+public class CandidateDto {
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
