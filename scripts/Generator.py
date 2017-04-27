a91808d2e992f6426b8b81219f1dfb555e9af26d
Olga <tedolga@gmail.com>
2017-04-27 21:22:40 +0300

    Position Generation

--- scripts/Generator.py
+++ scripts/Generator.py
@@ -49,15 +49,34 @@ def get_user_work_experience(university_start_year):
     work_experiences = []
     work_start_year = university_start_year + random.randint(4, 8)
     current_year = datetime.datetime.now().year
-    counter = work_start_year
-    while counter <= current_year:
+    year_counter = work_start_year
+    while year_counter <= current_year:
         work_experience = {}
-        work_experience['start_year'] = counter
-        counter = counter + random.randint(1, 15)
-        work_experience['current_job'] = 'true' if (counter > current_year) else 'false'
-        work_experience['end_year'] = counter if (counter <= current_year) else ''
+        work_experience['start_year'] = year_counter
+        year_counter = year_counter + random.randint(1, 15)
+        work_experience['current_job'] = 'true' if (year_counter > current_year) else 'false'
+        work_experience['end_year'] = year_counter if (year_counter <= current_year) else ''
         work_experience['company'] = get_random_file_line(companies_file)
         work_experiences.append(work_experience)
+    it_specialization = random.choices(it_specializations, weights=[7, 5, 2, 2])[0]
+    spec_postitions = get_random_file_line('dictionaries/' + it_specialization + '_positions.txt').split(',')
+    adv_spec_postitions = [] if (it_specialization == 'ba' or it_specialization == 'cm')else get_random_file_line(
+        'dictionaries/' + it_specialization + '_adv_positions.txt').split(',')
+    common_adv_postitions = [] if (it_specialization == 'cm') else get_random_file_line(
+        'dictionaries/common_adv_positions.txt').split(',')
+    positions = []
+    positions.extend(spec_postitions)
+    positions.extend(adv_spec_postitions)
+    positions.extend(common_adv_postitions)
+    while len(work_experiences) > len(positions):
+        positions_copy = positions
+        positions = []
+        for pos_counter in range(0, len(positions_copy)):
+            rand_count = random.randint(2, 4)
+            for copy_counter in range(0, rand_count):
+                positions.append(positions_copy[pos_counter])
+    for exp_counter in range(0, len(work_experiences)):
+        work_experiences[exp_counter]['position'] = positions[exp_counter]
     return work_experiences
 
 
@@ -66,6 +85,7 @@ first_names_file = 'dictionaries/first_names.csv'
 last_names_file = 'dictionaries/last_names.csv'
 university_file = 'dictionaries/universities.csv'
 companies_file = 'dictionaries/companies.csv'
+it_specializations = ['dev', 'test', 'ba', 'cm']
 for counter in range(1, 10):
     user = {}
     user['first_name'] = get_random_file_line(first_names_file)
